ec767cc06ac44744d4e39f55204df6be6da4aec2
TnIoP <pointblank94@naver.com>
2023-05-25 11:48:47 +0900

    fix typo

--- board/src/main/java/com/h2o/board/comment/dto/CommentDTO.java
+++ board/src/main/java/com/h2o/board/comment/dto/CommentDTO.java
@@ -11,13 +11,13 @@ public class CommentDTO {
     private int id;
     private String ip;
     private String contents;
-    private int parent_comment_id;
+    private int parentCommentId;
     private int depth;
-    private Boolean is_hide;
+    private Boolean isHide;
 
     @JsonFormat(pattern = "YYYY-MM-dd HH:mm:ss")
     private Timestamp createdAt;
     @JsonFormat(pattern = "YYYY-MM-dd HH:mm:ss")
     private Timestamp updatedAt;
-    private int post_id;
+    private int postId;
 }
